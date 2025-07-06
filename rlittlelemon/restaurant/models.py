3bd1792b172b9f7b5e3802c469001a945e8c01c9
gabrielaamel <gabotti0@gmail.com>
2025-07-06 09:27:49 -0400

    2 commit
    models

--- rlittlelemon/restaurant/models.py
+++ rlittlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=100)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.Title} - {self.Price}'
+    
+class Booking(models.Model):
+    Name = models.CharField(max_length=100)
+    NO_of_guests = models.IntegerField()
+    BookingDate = models.DateField()
+    BookingTime = models.TimeField()
+
+    def __str__(self):
+        return f'{self.Name} - {self.Date} at {self.Time}'
