79effa99d7dd95cd73ab7d5e09bd33c431a7d14e
gabrielaamel <gabotti0@gmail.com>
2026-05-25 07:26:46 -0400

    Fix :correct import reference for obtain_auth_token

--- rlittlelemon/restaurant/urls.py
+++ rlittlelemon/restaurant/urls.py
@@ -1,22 +1,20 @@
-from rest_framework.authtoken.views import obtain_auth_token
-from django.urls import path, include
-from . import views
-from  rest_framework.routers import DefaultRouter
-# Register the BookingViewSet
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token  # You have this
+from . import views # And this
+from rest_framework.routers import DefaultRouter
 
-router= DefaultRouter()
-router.register(r'tables', views.BookingViewSet )
-
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('users/', views.UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
-    path('api-token-auth/', views.obtain_auth_token), # Ensure this is imported correctly from rest_framework
+
+    # REMOVE the 'views.' prefix here:
+    path('api-token-auth/', obtain_auth_token),
 ] + router.urls
-
-
 
 
 
