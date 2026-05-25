42abe9b33f2d5fcedb17f12747c15437aaedde5a
gabrielaamel <gabotti0@gmail.com>
2026-05-25 07:03:55 -0400

    Refactor: flatten URL routing for React integration

--- rlittlelemon/restaurant/urls.py
+++ rlittlelemon/restaurant/urls.py
@@ -2,22 +2,19 @@ from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 from . import views
 from  rest_framework.routers import DefaultRouter
- # Register the BookingViewSet
+# Register the BookingViewSet
 
 router= DefaultRouter()
 router.register(r'tables', views.BookingViewSet )
 
 
 urlpatterns = [
-    path('', views.index, ),
+    path('', views.index, name='index'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('users/', views.UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
-    path('api-token-auth/', obtain_auth_token),
-    path('booking/',include(router.urls))
-
-]
-
+    path('api-token-auth/', views.obtain_auth_token), # Ensure this is imported correctly from rest_framework
+] + router.urls
 
 
 
--- rlittlelemon/rlittlelemon/urls.py
+++ rlittlelemon/rlittlelemon/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for rlittlelemon project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 #from rest_framework.routers import DefaultRouter
@@ -28,13 +13,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 # The router will automatically create the necessary routes for the BookingViewSet
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
+path('admin/', admin.site.urls),
+    # This brings in everything from your restaurant app's urls.py
+    # keeping the API routes clean and under the 'api/' prefix.
     path('api/', include('restaurant.urls')),
-    path('', include('restaurant.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # URL for obtaining auth token
-    
-    path('auth/', include('djoser.urls')),  # Include Djoser's authentication URLs
-    path('auth/', include('djoser.urls.authtoken')),  # Include Djoser's token authentication URLs
-    
+
+    # Djoser endpoints for authentication
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+
 
 ]
